78bbc54453c8e27870d8cc993a79242860070d47
thanhedward <ngocthanh12082003d@gmail.com>
2024-02-10 11:42:30 +0700

    fix

--- backend/core/probability_reward.py
+++ backend/core/probability_reward.py
@@ -2,11 +2,11 @@ from helpers.enums import EReward
 # change if reward has adjustment
 # sum must be 1
 netflix = 0 #0%
-m100k = 0.015 #1.5%
-m10k = 0.1 #10%
-m5k = 0.055 #5.5%
-m1k = 0.2 #20%
-none = 0.63 #63%
+m100k = 0 #1.5%
+m10k = 0 #10%
+m5k = 0 #5.5%
+m1k = 0 #20%
+none = 1 #63%
 max_config_obj = {
     EReward.NETFLIX: 0,
     EReward.MONEY100: 2,
